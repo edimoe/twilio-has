854b0fddc84e5112f1f8da773e8ea2c7b3fbf2a7
JUMAEDI SAPUTRA <acokhook@gmail.com>
2025-03-05 20:13:24 +0800

    Create publish_haddock.sh

---
+++ script/publish_haddock.sh
@@ -0,0 +1,38 @@
+set -e
+
+user='markandrus'
+repo='twilio-haskell'
+package='twilio'
+
+if [[ "$BUILD" == "stack" ]]
+then
+  exit 0
+fi
+
+if [[ "${TRAVIS_REPO_SLUG}"  == "${user}/${repo}" \
+   && "${TRAVIS_PULL_REQUEST}" == 'false' \
+   && "${TRAVIS_BRANCH}" == 'master' ]]
+then
+
+  echo "Generating Haddock documentation..."
+  cabal haddock
+  cp -R dist/doc/html/${package} ${HOME}/doc
+
+  echo "Cloning repo..."
+  cd ${HOME}
+  git config --global user.email 'travis@travis-ci.org'
+  git config --global user.name 'travis-ci'
+  git clone --quiet --branch=gh-pages \
+    https://${HADDOCK_KEY}@github.com/${user}/${repo} gh-pages >/dev/null
+  
+  echo "Updating repo..."
+  cd gh-pages
+  rm -rf *
+  mv ../doc/* .
+  git add -f .
+  git commit -m "Updating Haddock documentation (${TRAVIS_BUILD_NUMBER})"
+  git push -fq origin gh-pages >/dev/null
+  
+  echo "Published Haddock documentation."
+
+fi
